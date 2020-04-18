00845aac27e7ebce20ca4ffc21f2b7b0ee4afd28
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-04-18 17:49:59 -0300

    First curve

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,21 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Curve } from '../.';
+
+const knobs: [number, number][] = [
+  [5, 50],
+  [50, 80],
+  [100, 60],
+  [150, 10],
+  [195, 20],
+];
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <svg width="200" height="100" xmlns="http://www.w3.org/2000/svg">
+      <Curve knobs={knobs} fill="transparent" stroke="black" />
+    </svg>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
+import * as d3 from 'd3-shape';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+type CurveProps = { knobs: [number, number][] } & React.SVGProps<
+  SVGPathElement
+>;
+
+export function Curve({ knobs, ...props }: CurveProps) {
+  const d = d3.line().curve(d3.curveCatmullRom.alpha(0.5))(knobs) || '';
+  // const d = cubicPath(knobs);
+  return (
+    <>
+      <path d={d} {...props} />
+      {knobs.map(([x, y], i) => (
+        <circle cx={x} cy={y} r="5" fill="red" key={i} />
+      ))}
+    </>
+  );
+}
